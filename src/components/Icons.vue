670c7e530d12aba41ab175532cd08b0cb1df7e6c
fuzhongyu <fuzhongyu2@163.com>
2018-12-20 21:09:50 +0800

    update

---
+++ src/components/Icons.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
+            </div>
+            <p class="icon-text">景点门票</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Icons"
+    }
+</script>
+
+<!--scoped使样式只对当前组件生效-->
+<style lang="stylus" scoped>
+    .icons
+        padding-bottom 50%
+        background bisque
+        .icon
+            float left
+            overflow hidden
+            width 25%
+            height 0
+            padding-bottom 25%
+            background darkgrey
+            .icon-img-content
+                width 100%
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <icons></icons>
   </div>
 </template>
 
 <script>
 
-import Header from "../components/Header"
-import Swip from "../components/Swip"
+import Header from "../components/Header";
+import Swip from "../components/Swip";
+import Icons from "../components/Icons";
 
 export default {
   name: "home",
   components: {
       HomeHeader: Header,
-      HomeSwiper: Swip
+      HomeSwiper: Swip,
+      Icons: Icons
   }
 };
 </script>
